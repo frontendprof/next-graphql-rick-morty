3be765e189c7ee0efec5e98da77196762791c4d4
frontendprof <https://github.com/frontendprof>
2021-07-27 18:34:20 +0500

    Initial setup

--- pages/index.js
+++ pages/index.js
@@ -67,3 +67,45 @@ export default function Home() {
     </div>
   )
 }
+export async function getStaticProps(){
+  const client= new ApolloClient({
+    uri:"https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache()
+  })
+  const {data}=await client.query({
+    query:gql`
+      query{
+        characters(page:1){
+          info{
+            count
+            pages
+          }
+          results{
+            name
+            id
+            location{
+              id
+              name
+            }
+            origin{
+              id
+              name
+            }
+            episode{
+              id
+              episode
+              air_date
+            }
+            image
+          }
+        }
+      }
+    `
+  })
+  return{
+    props:{
+      characters:data.characters.results,
+      
+    }
+  }
+}
