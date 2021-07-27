0d5a6ddd19dbf34e0372036e0ffbf4022a8e6ad0
frontendprof <https://github.com/frontendprof>
2021-07-27 20:33:48 +0500

    Search characters api config

---
+++ pages/api/SearchChars.js
@@ -0,0 +1,56 @@
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    // ApolloProvider,
+    // useQuery,
+    gql
+  } from "@apollo/client";
+
+
+  const client=new ApolloClient({
+      uri:"https://rickandmortyapi.com/graphql",
+      cache:InMemoryCache()
+  })
+
+  export default async (req,res)=>{
+      const search=req.body
+      try {
+          const {data}=await client.query({
+              query:gql`
+                query{
+                    characters(filter:{name:"${search}"}){
+                    info{
+                        count
+                        pages
+                    }
+                    results{
+                        name
+                        id
+                        location{
+                        id
+                        name
+                        }
+                        origin{
+                        id
+                        name
+                        }
+                        episode{
+                        id
+                        episode
+                        air_date
+                        }
+                        image
+                    }
+                    }
+                }
+              `
+          })
+          res.status(200).json({characters:data.characters.results, error:null})
+      }
+      catch (error) {
+          if(error.message==="404: Not Found"){
+              res.status(400).json({characters:null,error:"No character found"})
+          }
+      }
+  }
